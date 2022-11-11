2209fd075d00842b0aa0f91c453fbff359e17b94
Cristian Baldi <baddy227@gmail.com>
2022-11-11 23:48:46 +0100

    add turn off leds

---
+++ TurnOffLeds/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+void exec(string cmd)
+{
+    var ps = new ProcessStartInfo(cmd)
+    {
+        UseShellExecute = true,
+        Verb = "open"
+    };
+    Process.Start(ps);
+}
+
+//string escape = "^&";
+
+string install = "signalrgb://effect/install/Solid%20Color";
+string apply = "signalrgb://effect/apply/Solid%20Color?color=0";
+
+if (args.Length > 0 && args[0].Equals("-i"))
+{
+    exec(install);
+}
+else
+{
+    exec(apply);
+}
