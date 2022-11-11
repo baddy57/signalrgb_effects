f8ee21a7bc331d617788a28f1b9c547fcb747d4e
Cristian Baldi <baddy227@gmail.com>
2022-11-11 21:10:04 +0100

    Add project files.

---
+++ signalrgb_effects/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
+string appdatalocal = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+var dir = Path.Combine(appdatalocal, @"WhirlwindFX\SignalRgb\cache\effects");
+
+var fxDirs = Directory.GetDirectories(dir);
+
+List<string> fxs = new();
+
+foreach (var fxDir in fxDirs)
+{
+    var filePath = Path.Combine(fxDir, "effect.html");
+    if (File.Exists(filePath))
+    {
+        var text = File.ReadAllText(filePath);
+        var tag = Regex.Match(text, "<title>.*</title>").Value;
+        var title = tag.Replace("<title>", "");
+        title = title.Replace("</title>", "");
+        title = title.Replace(" ", "%20");
+        fxs.Add(title);
+        Console.WriteLine(title);
+    }
+}
+var outPath = @"c:\script\effects.txt";
+File.WriteAllLines(outPath, fxs);
